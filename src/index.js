89551f7ac4c1e84fc8e4bc22c8ab43b075d6d49b
Igor Pryazhentsev <igor.pryazhenstev@mail.ru>
2022-01-08 20:42:43 +0300

    init: start reverse-int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var intString;
+    if (n < 0) {
+        intString = String(n).substring(1)
+    } else { intString = String(n) }
+    let intArray = intString.split("")
+    let reverseArray = intArray.reverse()
+    let reverseString = reverseArray.join("")
+    return Number(reverseString)
 }
